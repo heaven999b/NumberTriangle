ed51c74068752c59b02b19997e01341a433576f4
yihaiwen <yihaiwen9@gmail.com>
2025-09-23 03:12:33 +0800

    com

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -88,8 +88,26 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+        NumberTriangle cur = this;
+        if (path == null || path.isEmpty()) return cur.root;
+
+        for (int i = 0; i < path.length(); i++) {
+            char ch = path.charAt(i);
+            if (ch == 'l') {
+                if (cur.left == null) {
+                    throw new IllegalArgumentException();
+                }
+                cur = cur.left;
+            } else if (ch == 'r') {
+                if (cur.right == null) {
+                    throw new IllegalArgumentException();
+                }
+                cur = cur.right;
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
+        return cur.root;
     }
 
     /** Read in the NumberTriangle structure from a file.
